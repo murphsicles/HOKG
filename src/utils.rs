53f6f7fcabffb3a561d62dbfac8ce0c06919f1de
Roy Murphy <roycoins@gmail.com>
2025-06-27 14:05:50 +0100

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -59,11 +59,11 @@ fn extended_gcd(a: &BigInt, b: &BigInt) -> (BigInt, BigInt, BigInt) {
     while !r.is_zero() {
         let quotient = &old_r / &r;
         let temp_r = r.clone();
-        r = old_r - quotient * &r;
+        r = old_r - quotient.clone() * &r;
         old_r = temp_r;
 
         let temp_x = x.clone();
-        x = old_x - quotient * &x;
+        x = old_x - quotient.clone() * &x;
         old_x = temp_x;
 
         let temp_y = y.clone();
