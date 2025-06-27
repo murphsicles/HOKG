14e8b44904484008c5067b430c5c259de4bb42dd
Roy Murphy <roycoins@gmail.com>
2025-06-27 15:36:50 +0100

    Update point.rs

--- src/point.rs
+++ src/point.rs
@@ -2,9 +2,9 @@
 
 use num_bigint_dig::BigInt;
 
-// Represents a point on an elliptic curve, either as coordinates (x, y) or the point at infinity
+// Represents a point on an elliptic curve (x, y) or the point at infinity.
 #[derive(Clone, Debug, PartialEq)]
-pub enum Point {
-    Coordinates(BigInt, BigInt), // Point with x and y coordinates
-    Infinity,                    // Point at infinity
+pub enum-Point {
+    Infinity,
+    Coordinates(BigInt, BigInt),
 }
